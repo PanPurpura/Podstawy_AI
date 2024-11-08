25a0ba1ae3af7e20564f764f5452044791b6d54a
PanPurpura <82703680+PanPurpura@users.noreply.github.com>
2024-11-08 17:13:12 +0100

    feat: added functionality for matrix and pointToWorldSpace method

--- Project_1/VectorUtils.py
+++ Project_1/VectorUtils.py
@@ -14,3 +14,50 @@ class VectorUtils:
     @staticmethod
     def perp(vec):
         return engine.Vector2(-vec.y, vec.x)
+    
+    @staticmethod
+    def pointToWorldSpace(self, point, agent_heading, agent_side, agent_position):
+        point_copy = engine.Vector2(point.x, point.y)
+
+        mat_transform = [[1, 0, 0],
+                         [0, 1, 0],
+                         [0, 0, 1]]
+        
+        mat_transform = self.rotate_mat(mat_transform, agent_heading, agent_side)
+        mat_transform = self.translate_mat(mat_transform, agent_position.x, agent_position.y)
+        point_copy = self.transformVector(mat_transform, point_copy)
+
+        return point_copy
+
+        
+    def rotate_mat(self, matrix, heading, side):
+
+        copy_mat = matrix
+
+        copy_mat[0][0] = heading.x
+        copy_mat[0][1] = heading.y
+        copy_mat[1][0] = side.x
+        copy_mat[1][1] = side.y
+
+        return copy_mat
+    
+    def translate_mat(self, matrix, x, y):
+
+        copy_mat = matrix
+
+        copy_mat[0][2] = x
+        copy_mat[1][2] = y
+
+        return copy_mat
+    
+    def transformVector(self, matrix, point):
+
+        copy_point = point
+
+        x = (matrix[0][0] * copy_point.x) + (matrix[0][1] * copy_point.y) + matrix[0][2]
+        y = (matrix[1][0] * copy_point.x) + (matrix[1][1] * copy_point.y) + matrix[1][2]
+
+        copy_point.x = x
+        copy_point.y = y
+
+        return copy_point
